84adf58d9a97d765d7d69f35f93fc751ec4be18f
Mike Houser <saintmikel2004@gmail.com>
2020-06-26 12:18:07 -0400

    React!

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// .register() will allow Offline and load faster. 
+// made debuging more difficult... I found out.
+// 
+//serviceWorker.unregister();
+
+serviceWorker.register();
